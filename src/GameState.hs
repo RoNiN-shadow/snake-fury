182171ffbff2cbf77f2a76c9a4b8935e5ba014cc
Mark <marktishuk@gmail.com>
2026-03-27 18:02:20 +0100

    mv test from comment to Spec

--- src/GameState.hs
+++ src/GameState.hs
@@ -70,16 +70,6 @@ inSnake p s =
     checkTail = p `elem` snakeBody s
   in checkH || checkTail
 
-{-
-This is a test for inSnake. It should return 
-True
-True
-False
--}
--- >>> snake_seq = SnakeSeq (1,1) (Data.Sequence.fromList [(1,2), (1,3)])
--- >>> inSnake (1,1) snake_seq
--- >>> inSnake (1,2) snake_seq
--- >>> inSnake (1,4) snake_seq
 
 -- | Calculates de new head of the snake. Considering it is moving in the current direction
 --   Take into acount the edges of the board
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,7 @@
 
 import GameState
 import Test.Hspec
+import Data.Sequence (fromList)
 
 opositeMovementSpec :: Spec
 opositeMovementSpec = do
@@ -14,7 +15,19 @@ opositeMovementSpec = do
       it "returs East when given West" $ do
         opositeMovement West `shouldBe` East
 
+inSnakeSpec :: Spec
+inSnakeSpec = do
+  describe "Check if a point is in the snake" $ do
+    let snake_seq = SnakeSeq (1,1) (fromList [(1,2), (1,3)])
+    it "returns True if the point is in the snake" $ do
+      inSnake (1,1) snake_seq `shouldBe` True
+    it "returns True if the point is in the snake" $ do
+      inSnake (1,2) snake_seq `shouldBe` True
+    it "returns False if the point is not in the snake" $ do
+      inSnake (1,4) snake_seq `shouldBe` False
+
+
 main :: IO ()
 main = hspec $ do
     opositeMovementSpec
-      
+    inSnakeSpec
